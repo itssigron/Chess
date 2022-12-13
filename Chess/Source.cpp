4e46492fab7a8a0b6e74bec5a11607e1603dc75b
Harel Sigron <arel1234123@gmail.com>
2022-12-14 00:21:46 +0200

    Added the Board class into our main file, and implemented main logic for the piece-move operation

--- Chess/Source.cpp
+++ Chess/Source.cpp
@@ -1,26 +1,25 @@
 /*
 This file servers as an example of how to use Pipe.h file.
-It is recommended to use the following code in your project, 
+It is recommended to use the following code in your project,
 in order to read and write information from and to the Backend
 */
 
 #include "Pipe.h"
 #include <iostream>
 #include <thread>
+#include "Board.h"
 
 using std::cout;
 using std::endl;
 using std::string;
 
 
-void main()
+int main()
 {
 	srand(time_t(NULL));
-
-	
 	Pipe p;
 	bool isConnect = p.connect();
-	
+
 	string ans;
 	while (!isConnect)
 	{
@@ -34,20 +33,17 @@ void main()
 			Sleep(5000);
 			isConnect = p.connect();
 		}
-		else 
+		else
 		{
 			p.close();
-			return;
+			return 0;
 		}
 	}
-	
 
-	char msgToGraphics[1024];
-	// msgToGraphics should contain the board string accord the protocol
-	// YOUR CODE
 
-	strcpy_s(msgToGraphics, "rnbkqbnrpppppppp################################PPPPPPPPRNBKQBNR1"); // just example...
-	
+	char msgToGraphics[1024] = "rnbkqbnrpppppppp################################PPPPPPPPRNBKQBNR1";
+	Board board = Board(string(msgToGraphics)); //initialize our Board class
+
 	p.sendMessageToGraphics(msgToGraphics);   // send the board string
 
 	// get message from graphics
@@ -55,18 +51,31 @@ void main()
 
 	while (msgFromGraphics != "quit")
 	{
-		// should handle the string the sent from graphics
-		// according the protocol. Ex: e2e4           (move e2 to e4)
-		
-		// YOUR CODE
-		strcpy_s(msgToGraphics, "YOUR CODE"); // msgToGraphics should contain the result of the operation
+		//access src and dest pieces using the information from the client
+		Piece& srcPiece = board.getPiece(msgFromGraphics.substr(0, 2));
+		Piece& destPiece = board.getPiece(msgFromGraphics.substr(2, 2));
+
+		// make basic checks, if all basic checks passed, make
+		// further checks with the current piece, and send result to graphics
+		int result = srcPiece.basicValidateMove(board.getCurrentPlayer(), destPiece);
+		if (result == VALID_MOVE) result = srcPiece.move(destPiece);
+
+		if (result == VALID_MOVE)
+		{
+			// update source piece location
+			board.setPiece(srcPiece, destPiece);
+		}
+
+		msgToGraphics[0] = result + '0';
+		msgToGraphics[1] = 0; //end string
 
 		// return result to graphics		
-		p.sendMessageToGraphics(msgToGraphics);   
+		p.sendMessageToGraphics(msgToGraphics);
 
 		// get message from graphics
 		msgFromGraphics = p.getMessageFromGraphics();
 	}
 
 	p.close();
+	return 0;
 }
