a4e9cf8c6d87336d6b02b212a06eeb7a8c409a82
Harel Sigron <arel1234123@gmail.com>
2022-12-14 22:53:54 +0200

    removed useless methods

--- Chess/Piece.cpp
+++ Chess/Piece.cpp
@@ -50,65 +50,8 @@ int Piece::basicValidateMove(Player& currentPlayer, Piece& dest)
 		result = INVALID_DEST_PLAYER; // current player cant kill his own piece
 	}
 
-	std::cout << "A move from " << _location << " to " << dest._location
-		<< " resulted in code " << result << std::endl;
-
 	//finished all basic checks, return final result
 	return result;
-}
-
-int Piece::validateVertically(Piece& dest)
-{
-	char srcRow = _location[1], srcCol = _location[0];
-	char destRow = dest.getLocation()[1], destCol = dest.getLocation()[0];
-	bool foundPiece = false; // loop flag to whether we found a non-empty piece in the rook's path
-	char i = 0;
-	int bigger = 0, lower = 0;
-
-	 // if its not vertically, return invalid move
-	if (srcCol != destCol) return INVALID_PIECE_MOVE;
-
-	bigger = srcRow > destRow ? srcRow : destRow;
-	lower = bigger == srcRow ? destRow : srcRow;
-	// loop through all pieces in the path (excluding src and dest pieces)
-	// we dont need to check for src and dest because it was already validated
-	for (i = lower + 1; i < bigger && !foundPiece; i++)
-	{
-		// if there is a piece in the rook's path, move is invalid.
-		if (_owner->getBoard().getPiece(string(1, srcCol) + i).getType() != EMPTY_PIECE)
-		{
-			foundPiece = true;
-		}
-	}
-
-	return foundPiece ? INVALID_PIECE_MOVE : VALID_MOVE;
-}
-
-int Piece::validateHorizontally(Piece& dest)
-{
-	char srcRow = _location[1], srcCol = _location[0];
-	char destRow = dest.getLocation()[1], destCol = dest.getLocation()[0];
-	bool foundPiece = false; // loop flag to whether we found a non-empty piece in the rook's path
-	char i = 0;
-	int bigger = 0, lower = 0;
-
-	// if its not horizontally, return invalid move
-	if (srcRow != destRow) return INVALID_PIECE_MOVE;
-
-	bigger = srcCol > destCol ? srcCol : destCol;
-	lower = bigger == srcCol ? destCol : srcCol;
-	// loop through all pieces in the path (excluding src and dest pieces)
-	// we dont need to check for src and dest because it was already validated
-	for (i = lower + 1; i < bigger && !foundPiece; i++)
-	{
-		// if there is a piece in the rook's path, move is invalid.
-		if (_owner->getBoard().getPiece(string(1, i) + srcRow).getType() != EMPTY_PIECE)
-		{
-			foundPiece = true;
-		}
-	}
-
-	return foundPiece ? INVALID_PIECE_MOVE : VALID_MOVE;
 }
 
 std::ostream& operator<<(std::ostream& os, const Piece& obj)
