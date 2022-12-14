6f653bb958c1d9104b911e2b230db40d3326ea22
itssigron <arel1234123@gmail.com>
2022-12-14 10:44:00 +0200

    Added access to board in player

--- Chess/Player.cpp
+++ Chess/Player.cpp
@@ -3,18 +3,25 @@
 Player::Player()
 {
 	_type = WHITE_PLAYER;
+	_board = nullptr;
 }
 
-Player::Player(int type)
+Player::Player(Board* board, int type)
 {
 	if (type != WHITE_PLAYER && type != BLACK_PLAYER)
 	{
 		//todo: throw an error
 	}
 	_type = type;
+	_board = board;
 }
 
 int Player::getType() const
 {
 	return _type;
+}
+
+Board& Player::getBoard()
+{
+	return *_board;
 }
