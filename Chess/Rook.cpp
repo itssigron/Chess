6b39063a37fa5641f051ef0817a6c9973fdb72d5
itssigron <arel1234123@gmail.com>
2022-12-14 10:41:53 +0200

    Fully implemented the validateMove function

--- Chess/Rook.cpp
+++ Chess/Rook.cpp
@@ -5,7 +5,62 @@ Rook::Rook(Player* owner, string location) : Piece(owner, location, ROOK)
 
 }
 
-int Rook::move(Piece& dest)
+int Rook::validateMove(Piece& dest)
 {
+	char srcRow = _location[1];
+	char srcCol = _location[0];
+	char destRow = dest.getLocation()[1];
+	char destCol = dest.getLocation()[0];
+	bool foundPiece = false; // loop flag to whether we found a non-empty piece in the rook's path
+	char i = 0;
+	int bigger = 0, lower = 0;
+
+	if (srcRow == destRow) // horizontally
+	{
+		bigger = srcCol > destCol ? srcCol : destCol;
+		lower = bigger == srcCol ? destCol : srcCol;
+		// loop through all pieces in the path (excluding src and dest pieces)
+		// we dont need to check for src and dest because it was already validated
+		for (i = lower + 1; i < bigger && !foundPiece; i++)
+		{
+			// if there is a piece in the rook's path, move is invalid.
+			if (_owner->getBoard().getPiece(string(1, i) + srcRow).getType() != EMPTY_PIECE)
+			{
+				foundPiece = true;
+			}
+		}
+
+		if (foundPiece)
+		{
+			return INVALID_PIECE_MOVE;
+		}
+	}
+	else if (srcCol == destCol) // vertically
+	{
+		bigger = srcRow > destRow ? srcRow : destRow;
+		lower = bigger == srcRow ? destRow : srcRow;
+		// loop through all pieces in the path (excluding src and dest pieces)
+		// we dont need to check for src and dest because it was already validated
+		for (i = lower + 1; i < bigger && !foundPiece; i++)
+		{
+			// if there is a piece in the rook's path, move is invalid.
+			if (_owner->getBoard().getPiece(string(1, srcCol) + i).getType() != EMPTY_PIECE)
+			{
+				foundPiece = true;
+			}
+		}
+
+		if (foundPiece)
+		{
+			return INVALID_PIECE_MOVE;
+		}
+	}
+	else
+	{
+		// if he neither moves horizontally nor vertically, its an invalid move
+		return INVALID_PIECE_MOVE;
+	}
+
+	// if all checks passed, its a valid move
 	return VALID_MOVE;
 }
