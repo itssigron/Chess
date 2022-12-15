c08e4a0cd6c1d6fa89315b3a3d560bc41537695c
itssigron <arel1234123@gmail.com>
2022-12-16 00:32:19 +0200

    Added comments

--- Chess/King.cpp
+++ Chess/King.cpp
@@ -12,6 +12,8 @@ int King::validateMove(Piece& dest)
 	// dest row and col
 	int x2 = dest.getLocation()[0] - 'a', y2 = dest.getLocation()[1] - '0';
 
+	// a king can move 1 square to any direction,
+	// so as long the difference between src col, dest col and src row, dest row isnt above 1, its a valid move
 	if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1)
 	{
 		return VALID_MOVE;
