c1fe04f7cd39ab07808399a3de16ca27f432224e
Harel Sigron <arel1234123@gmail.com>
2022-12-15 09:31:14 +0200

    Modified code to correspond new version of Board

--- Chess/Source.cpp
+++ Chess/Source.cpp
@@ -49,6 +49,8 @@ int main()
 		}
 	}
 
+	ans.clear();
+
 
 	// the first message to the graphics will be our board
 	char msgToGraphics[1024] = "rnbkqbnrpppppppp################################PPPPPPPPRNBKQBNR1";
@@ -62,23 +64,27 @@ int main()
 	while (msgFromGraphics != "quit")
 	{
 		//access src and dest pieces using the information from the client
-		Piece& srcPiece = board.getPiece(msgFromGraphics.substr(0, 2));
-		Piece& destPiece = board.getPiece(msgFromGraphics.substr(2, 2));
+		Piece* srcPiece = board.getPiece(msgFromGraphics.substr(0, 2));
+		Piece* destPiece = board.getPiece(msgFromGraphics.substr(2, 2));
 
 		// print all valid moves this src can do to console for debugging
-		board.printAllValidLocations(srcPiece);
+		board.printAllValidLocations(*srcPiece);
 		
 		// make basic checks, if all basic checks passed, make
 		// further checks with the current piece, and send result to graphics
-		int result = srcPiece.basicValidateMove(board.getCurrentPlayer(), destPiece);
-		if (result == VALID_MOVE) result = srcPiece.validateMove(destPiece);
+		int result = srcPiece->basicValidateMove(board.getCurrentPlayer(), *destPiece);
+		if (result == VALID_MOVE) result = srcPiece->validateMove(*destPiece);
 
 		// perform the actual "move"
 		if (result == VALID_MOVE)
 		{
 			// update source piece location
-			board.movePiece(srcPiece, destPiece);
+			board.movePiece(*srcPiece, *destPiece);
 		}
+
+		// free pieces's memory after use
+		delete srcPiece;
+		delete destPiece;
 
 		// send the result code to graphics
 		// constants for the codes can be found in Piece.h
@@ -92,6 +98,9 @@ int main()
 		msgFromGraphics = p.getMessageFromGraphics();
 	}
 
+	// free program's used memory
 	p.close();
+	msgFromGraphics.clear();
+
 	return 0;
 }
