d249d65bb29742fd8aac4d0a382c7bd49f91bccd
Razkoo <merdok226@gmail.com>
2022-12-16 11:18:06 +0200

    knight validate move

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -7,5 +7,15 @@ Knight::Knight(Player* owner, string location) : Piece(owner, location, KNIGHT)
 
 int Knight::validateMove(Piece& dest)
 {
-	return VALID_MOVE;
+	// src row and col
+	int x1 = _location[0] - 'a', y1 = _location[1] - '0';
+	// dest row and col
+	int x2 = dest.getLocation()[0] - 'a', y2 = dest.getLocation()[1] - '0';
+
+	if ((abs(x2 - x1) == 1 && abs(y2 - y1) == 2) || (abs(x2 - x1) == 2 && abs(y2 - y1) == 1))
+	{
+		return VALID_MOVE;
+	}
+
+	return INVALID_PIECE_MOVE;
 }
