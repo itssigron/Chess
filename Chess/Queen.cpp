c86dfe470f52268e4db81ae17494347f1b72972b
itssigron <arel1234123@gmail.com>
2022-12-16 12:18:16 +0200

    removed unnecessary 'this'

--- Chess/Queen.cpp
+++ Chess/Queen.cpp
@@ -7,8 +7,8 @@ Queen::Queen(Player* owner, string location) : Piece(owner, location, QUEEN)
 
 int Queen::validateMove(Piece& dest)
 {
-	Bishop b = Bishop(this->_owner, this->_location);
-	Rook r = Rook(this->_owner, this->_location);
+	Bishop b = Bishop(_owner, _location);
+	Rook r = Rook(_owner, _location);
 
 	// check if either of the moves is correct (assuming VALID_MOVE is 0)
 	return b.validateMove(dest) & r.validateMove(dest);
