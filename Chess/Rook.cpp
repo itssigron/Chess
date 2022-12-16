94b9bdb5c94be4b32fa3c846fb8e1c9c7f475a7a
itssigron <arel1234123@gmail.com>
2022-12-16 12:52:58 +0200

    Fully optimized validateMove method

--- Chess/Rook.cpp
+++ Chess/Rook.cpp
@@ -5,42 +5,35 @@ Rook::Rook(Player* owner, string location) : Piece(owner, location, ROOK)
 
 }
 
-// todo: optimize this function
 int Rook::validateMove(Piece& dest)
 {
-	// src row and col
-	int x1 = _location[0] - 'a', y1 = _location[1] - '0';
-	// dest row and col
-	int x2 = dest.getLocation()[0] - 'a', y2 = dest.getLocation()[1] - '0';
+	int srcRow = _location[1] - '0', destRow = dest.getLocation()[1] - '0';
+	int srcCol = _location[0] - 'a', destCol = dest.getLocation()[0] - 'a';
 
-	int biggerX = x1 > x2 ? x1 : x2, smallerX = x1 < x2 ? x1 : x2;
-	int biggerY = y1 > y2 ? y1 : y2, smallerY = y1 < y2 ? y1 : y2;
-	int smaller = x1 == x2 ? smallerY : smallerX, bigger = x1 == x2 ? biggerY : biggerX;
-	
-	int i = 0;
-	int row = 0, col = 0;
-	bool foundPiece = false; // loop flag
-	string& board = _owner->getBoard().getBoard();
-
-	// if its neither horizontally nor vertically, return invalid move
-	if (x1 != x2 && y1 != y2)
+	// Rooks can only move horizontally or vertically, so if the row or column doesn't change
+	// then the move is invalid
+	if (srcRow != destRow && srcCol != destCol)
 	{
 		return INVALID_PIECE_MOVE;
 	}
 
-	// in the loop, smaller gets added with 1 because theres no need to check 
-	// for both src and dest pieces since they were already validated.
+	// Calculate offset for row and col (whether we move horizontally left/right or vertically up/down)
+	int rowOffset = (srcRow < destRow) ? 1 : ((srcRow > destRow) ? -1 : 0);
+	int colOffset = (srcCol < destCol) ? 1 : ((srcCol > destCol) ? -1 : 0);
+	int row = srcRow + rowOffset, col = srcCol + colOffset;
 
-	// validate both vertically and horizontally
-	for (i = smaller + 1; i < bigger && !foundPiece; i++)
-	{
-		// get row and col based on loop index, x and y's
-		x1 == x2 ? (row = BOARD_SIZE - i, col = x1) : (row = BOARD_SIZE - y1, col = i);
+	bool foundPiece = false;
 
-		// if pieace isnt empty, set it to true and return invalid move
-		foundPiece = board[Board::getIndex(row, col)] != EMPTY_PIECE;
+	// moves horizontally or vertically until a piece is found, if we found a piece then its an invalid move
+	while ((row != destRow || col != destCol) && !foundPiece) {
+		// if theres a piece in the rook's path, update flag accordingly
+		foundPiece =
+			_owner->getBoard().getBoard()[Board::getIndex(string(1, col + 'a') + (char)(row + '0'))] != EMPTY_PIECE;
+
+		// go on to the next square in the rook's path
+		row += rowOffset;
+		col += colOffset;
 	}
-	
-	// if a piece wasnt found in the middle of the rook's path, return valid move
+
 	return foundPiece ? INVALID_PIECE_MOVE : VALID_MOVE;
 }
