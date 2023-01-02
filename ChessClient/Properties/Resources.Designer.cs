ced0da427caf0a942c081e3678a6b72b586be6ec
itssigron <arel1234123@gmail.com>
2023-01-03 00:26:04 +0200

    Updated resources + rename output client exe to Chess.exe

--- ChessClient/Properties/Resources.Designer.cs
+++ ChessClient/Properties/Resources.Designer.cs
@@ -181,6 +181,16 @@ namespace ChessClient.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Chess {
+            get {
+                object obj = ResourceManager.GetObject("Chess", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap white_bishop_v1 {
